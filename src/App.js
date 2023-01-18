8f8f783b1a3ffaf6b2882f94c50d3b28b7eb47b8
Junior <andresjunior240498hehe@gmail.com>
2023-01-17 22:48:22 -0400

    practica-map-react

--- src/App.js
+++ src/App.js
@@ -5,52 +5,58 @@ import Photo1 from "./images/rey_sisebuto.png"
 import Photo2 from "./images/rey_leogivildo.png"
 import Photo3 from "./images/rey_atanagildo.png"
 import Photo4 from "./images/rey_incognito.png" */
-import {useState} from 'react';
+
 
 
 function App() {
 
   const reyes=[
     {
-      nombre:"Atanagildo",
-      color:"darkolivegreen",
-      precio:178
+        nombre:"Atanagildo",
+        reinado:15,
+        vacasComidas:9
     },{
-      nombre:"Ervigio",
-      color:"crimson",
-      precio:169
+        nombre:"Ervigio",
+        reinado:7,
+        vacasComidas:3
     },{
-      nombre:"Ataúlfo",
-      color:"peru",
-      precio:81
+        nombre:"Ataúlfo",
+        reinado:5,
+        vacasComidas:16
     },{
-      nombre:"Leogivildo",
-      color:"darkmagenta",
-      precio:126
+        nombre:"Leovigildo",
+        reinado:18,
+        vacasComidas:3
     },{
-      nombre:"Recesvinto",
-      color:"royalblue",
-      precio:141
+        nombre:"Sisebuto",
+        reinado:9,
+        vacasComidas:13
     },{
-      nombre:"Sisebuto",
-      color:"teal",
-      precio:69
+        nombre:"Recesvinto",
+        reinado:19,
+        vacasComidas:11
+    },{
+        nombre:"Teodorico",
+        reinado:33,
+        vacasComidas:12
     }
   ]
 
-  const [total,setTotal]=useState(0)
-  let [cantidad,setCantidad]=useState(reyes.length)
+
 
   return(
     <>
-    <h1>Total a pagar: {total}$</h1>
-    <div className="main-box">
-      {reyes.map(rey=><Cena nombre={rey.nombre} color={rey.color} precio={rey.precio} setTotal={setTotal}  />)}
+    <div className="card-container">
+      {reyes.map(rey=>{
+      return <Cena texto={`${rey.nombre.toUpperCase()} ha comido ${rey.vacasComidas} vacas en sus ${rey.reinado} años de reinado`}
+      img={rey.nombre.toLowerCase()}
+      />}
+      )}
     </div>
-
-    
     </>
   )
 }
 
 export default App;
+
+
--- src/components/Cena.js
+++ src/components/Cena.js
@@ -1,30 +1,13 @@
-import React, { useState } from 'react'
 import "../stylesheets/Cena.css"
 
-function Cena({nombre,color,precio,setTotal}) {  
-    const ruta="https://www.html6.es/img/rey_"
-    let imagen=`${ruta}${nombre.toLowerCase()}.png`
-    
-    const comprar=(e)=>{
-
-        setTotal(e=>e+precio)
-    
-        console.log(e.target.parentNode.parentNode.style.display="none")
-
-    }
-
-    
+function Cena({texto,img}) {  
 
     return (
         <>
-          <div className="rey" style={{backgroundColor:color}}>
-              <h1>{nombre}</h1>
-              <img src={imagen} alt="" />
-              <div className="precio">
-                <h2>{precio}$</h2>
-                <button onClick={comprar}>Comprar</button>
-              </div>
-          </div>  
+        <div className="card">
+          <p>{texto}</p>
+          <img src={`https://www.html6.es/img/rey_${img}`} alt="" />
+        </div>
         </>
     )
 }
